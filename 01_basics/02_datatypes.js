3a60b22fa9d48af6cc72ab1b9b3704bade328971
Aditya <adityarastogi620@gmail.com>
2024-01-09 02:05:19 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+//datatypes
+
+"use strict";  //treat all JS code as newer version
+
+// alert(3 + 3)  -- works on browser only
+
+//datatypes
+let name = "aditya"   //string
+let age = 20         //number
+let check = true    //boolean
+
+//bigint
+//null => standalone value
+//undefined
+//symbol => unique
+//objects
+
+
+//typeof keyword - tells the type of the variables
+console.log(typeof null);  // =>object
+console.log(typeof undefined);  // =>undefined
+
+
+
+
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-)
