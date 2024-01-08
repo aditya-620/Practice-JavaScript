1380d450340a705009454037137b48a880f7b0ec
Aditya <adityarastogi620@gmail.com>
2024-01-08 14:33:29 +0530

    variables

---
+++ 01_variables.js
@@ -0,0 +1,38 @@
+//variables in JavaScript
+
+//to print something
+console.log("aditya");
+
+//const - we cannot change the value of this
+const userId = 123456
+// userId = 456789  (not possible)
+
+//let - most popular way to declare a variable
+let userName = "aditya"
+let userEmail = "adityarastogi620@gmail.com"
+
+//var keyword
+var accountPassword = "6969"
+/*
+perfer not to use var
+because of issue in block scope and functional scope
+*/
+
+//Javascript also allow to declare variables without writing anything before it
+userCity = "Lucknow"
+//perfer not to use
+
+//JavaScript say undefined to the variables with no value
+let userState
+
+
+console.log(userId);
+
+//we can print all the variable using table
+console.table([userId, userName, userEmail, accountPassword, userCity, userState])
+
+//Note: you can use semicolon(;) to terminate the line, but it is not necessary
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-)
