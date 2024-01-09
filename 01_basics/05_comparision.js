8fde38f61c974d3dd837ef20179dc76c4b40143b
Aditya <adityarastogi620@gmail.com>
2024-01-09 14:46:28 +0530

    comparision

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,24 @@
+//comparision operators gives out as true and false
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("---------------------");
+console.log("2" > 1);
+console.log("02" > 1);
+console.log("---------------------");
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log("---------------------");
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log("---------------------");
+// ===  -> strict comparision
+console.log("2" === 2);
+
+//If you like my code do give stars:-)
+//Keep coding!!
+//Thankyou;-)
