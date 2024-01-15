13ec77729be0bec099b935056117037293dd42d2
Aditya <adityarastogi620@gmail.com>
2024-01-16 01:16:46 +0530

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,27 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// (function () {
+//     // …
+//   })();
+  
+//   (() => {
+//     // …
+//   })();
+  
+//   (async () => {
+//     // …
+//   })();
+//IIFE is basically used to remove the global scope pollution on that particular function
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();  //we have to use the semi-colon to end the IIFE
+
+( () => {
+    console.log(`DB CONNECTED TWO`);
+} )();
+
+( (name) => {
+    console.log(`DB CONNECTED THREE ${name}`);
+} )('ADITA');
