e748373ec3334d9df57377d4bc503a184366a516
Aditya <adityarastogi620@gmail.com>
2024-01-16 22:51:12 +0530

    if statements

--- 04_control_flow/01_if_statements.js
+++ 04_control_flow/01_if_statements.js
@@ -2,43 +2,44 @@
 const isUserloggedIn = true
 const temperature = 41
 
-// if ( temperature === 40 ){
-//     console.log("less than 50");
-// } else {
-//     console.log("temperature is greater than 50");
-// }
+if ( temperature === 40 ){   //strict comparision
+    console.log("less than 50");
+} else {
+    console.log("temperature is greater than 50");
+}
 
 // console.log("Execute");
 //--------------------------------------------------------
 //comparisions operator
 // <, >, <=, >=, ==, !=, ===, !==
 
-// const score = 200
+const score = 200
 
-// if (score > 100) {
-//     let power = "fly"
-//     console.log(`User power: ${power}`);
-// }
+if (score > 100) {
+    let power = "fly"
+    console.log(`User power: ${power}`);
+}
 
-// console.log(`User power: ${power}`);
+// console.log(`User power: ${power}`);    //out of scope
+//--------------------------------------------------------
 
-
-// const balance = 1000
-
+//impilicit (not a good practice)
 // if (balance > 500) console.log("test"),console.log("test2");
 
-// if (balance < 500) {
-//     console.log("less than 500");
-// } else if (balance < 750) {
-//     console.log("less than 750");
-    
-// } else if (balance < 900) {
-//     console.log("less than 750");
-    
-// } else {
-//     console.log("less than 1200");
 
-// }
+const balance = 1000
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+    
+} else if (balance < 900) {
+    console.log("less than 750");
+    
+} else {
+    console.log("less than 1200");
+
+}
 
 const userLoggedIn = true
 const debitCard = true
